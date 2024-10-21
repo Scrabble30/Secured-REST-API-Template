83be1b01d6bb9494e3cd978a651210a90cdf4b12
Scrabble30 <144219106+Scrabble30@users.noreply.github.com>
2024-10-21 11:18:24 +0200

    Format logged error messages

--- src/main/java/app/controllers/ExceptionController.java
+++ src/main/java/app/controllers/ExceptionController.java
@@ -12,14 +12,14 @@ public class ExceptionController {
     private final Logger logger = LoggerFactory.getLogger(ExceptionController.class);
 
     public void handleAPIExceptions(APIException e, Context ctx) {
-        logger.error("{} {}", HttpStatus.forStatus(e.getStatusCode()), e.getMessage());
+        logger.error("{} - {}", HttpStatus.forStatus(e.getStatusCode()), e.getMessage());
 
         ctx.status(e.getStatusCode());
         ctx.json(new HttpMessageDTO(e.getStatusCode(), e.getMessage()));
     }
 
     public void handleExceptions(Exception e, Context ctx) {
-        logger.error("Unhandled exception: {} {}", e.getClass().getSimpleName(), e.getMessage());
+        logger.error("Unhandled exception: {} - {}", e.getClass().getSimpleName(), e.getMessage());
 
         ctx.status(HttpStatus.INTERNAL_SERVER_ERROR);
         ctx.json(new HttpMessageDTO(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage()));
